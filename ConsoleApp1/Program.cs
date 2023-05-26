b5cccb4206ff38998583bc63108cab4df6a60876
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2023-05-26 12:16:51 +0200

    Assigment 2 is klaar , Ik heb twee klassen gemaakt , Trainer en Pokeball , de charmendar zit in de Pokeball en de Trainer heeft 6 Pokeballen. Ik heb ook battle system gemaakt tussen twee Trainers.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,6 +16,9 @@ namespace Pokemone
 
             string answer = Console.ReadLine();
 
+
+
+        
             if (answer == "yes" || answer == "Yes")
             {
 
@@ -24,45 +27,45 @@ namespace Pokemone
 
                 while (quite == false)
                 {
-                    string newNameAnswer = "yes";
-                    string name = "";
 
-                    if (newNameAnswer == "yes" || newNameAnswer == "Yes")
+
+                    Console.WriteLine("Give a name for Trainer 1 : ");
+                    string trainer1Name = Console.ReadLine();
+
+                    Console.WriteLine("Give a name for Trainer 2: ");
+                    string trainer2Name = Console.ReadLine();
+
+
+                    Trainer trainer1 = new Trainer(trainer1Name);
+                    Trainer trainer2 = new Trainer(trainer2Name);
+                     
+
+                    for (int i = 0; i < 6 ; i++)
                     {
-                        Console.WriteLine("Give a name for your Charmender : ");
-                        name = Console.ReadLine();
+
+                        Console.WriteLine(trainer1.name + " turn : ");
+                        trainer1.throwPokeball(i);
+
+                        Console.WriteLine(trainer2.name +  " turn : ");
+                        trainer2.throwPokeball(i);
+
+                        Console.WriteLine(trainer1.name + "  : " );
+                        trainer1.returnPokeball();
+
+                        Console.WriteLine(trainer2.name +  "   : ");
+                        trainer2.returnPokeball();
 
                     }
 
-
-                    Charmander chamerendar = new Charmander(name, "fire", "water ");
-
-                    for (int i = 0; i < 10; i++)
-                    {
-                        chamerendar.battleCry();
-
-                    }
-
-                    Console.WriteLine("Do you want to stop ? : ( yse/no ) ");
+                    Console.WriteLine("Do you want to rstart ? : ( yse/no ) ");
                     string quitAnswer = Console.ReadLine();
-                    if (quitAnswer == "yes" || quitAnswer == "Yes")
+                    if (quitAnswer == "no" || quitAnswer == "No")
                     {
                         quite = true;
                         break;
                     }
-
-                    Console.WriteLine("Do you want to give your Charmender a new name ? : ( yse/no ) ");
-                    newNameAnswer = Console.ReadLine();
-
-
                 }
-
-
             }
-
-
-
-
         }
 
         class Charmander
@@ -85,6 +88,83 @@ namespace Pokemone
             }
         }
 
+
+        class Pokeball
+        {
+            public bool isEmpety;
+            public Charmander charmander;
+
+
+            public Pokeball(bool isEmpety, string name, string strength, string weakness)
+            {
+                this.isEmpety = isEmpety;
+
+                if (this.isEmpety == true)
+                {
+                    this.charmander = new Charmander (name, strength , weakness );
+ 
+                }
+            }
+
+            public void releasCharmander()
+            {
+                this.charmander.battleCry();
+            }
+
+            public void closePokeball()
+            {
+                Console.WriteLine(" Charmendar is back his Pokeball !");
+            }
+        }
+
+        class Trainer
+        {
+             
+            public string name;
+            public int pokeBallnum;
+            public Pokeball pokeball;
+            List<Pokeball> belt = new List<Pokeball>();
+            
+
+
+            public Trainer( string name)
+            {
+
+                this.name = name;
+                this.addtoBelt();
+            }
+
+            public void addtoBelt()
+            {
+
+                for (int i = 0; belt.Count < 6; i++)
+                {
+                    pokeball = new Pokeball (true , "charmendar" , "fire", "water ");
+                    belt.Add(pokeball);
+
+                }
+
+            }
+            
+
+            public void throwPokeball(int pokeBallnum)
+            {
+
+                this.pokeBallnum = pokeBallnum;
+                belt[pokeBallnum].releasCharmander();
+
+            }
+
+            public void returnPokeball()
+            {
+
+                pokeball.closePokeball();
+                Console.WriteLine(" Pokeball back to  belt ");
+
+
+
+            }
+        }
     }
 }
 
